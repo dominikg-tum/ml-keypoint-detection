232d389810768c16e38bde56f9d12713e133a9a7
dominikg-tum <dgarstenauer0@gmail.com>
2024-01-17 19:07:37 +0100

    Added reverse proxy + dev mode for easier development

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from reverseProxy import proxyRequest
+
+MODE = os.getenv('FLASK_ENV')
+DEV_SERVER_URL = 'http://localhost:3000/'
 
 app = Flask(__name__)
 
+# Ignore static folder in development mode.
+if MODE == "development":
+    app = Flask(__name__, static_folder=None)
+
 @app.route('/')
-def index():
-    return "Hello from FLASK"
+@app.route('/<path:path>')
+def index(path=''):
+    if MODE == 'development':
+        return proxyRequest(DEV_SERVER_URL, path)
+    else:
+        return render_template("index.html") 
